e2b5dcd0dac73a54ad770ebdc2cd38a444a189eb
franz <franz@doola.com>
2022-02-17 02:49:15 +0100

    build: update build

---
+++ build/precache-manifest.067f014611a3c5053756c4046d8787b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dd1ab71e75501a05959019341f8a60",
+    "url": "https://franz101.github.io/reactHN/build/index.html"
+  },
+  {
+    "revision": "1f6808fa73c0b8530363",
+    "url": "https://franz101.github.io/reactHN/build/static/css/main.5ebd29a5.chunk.css"
+  },
+  {
+    "revision": "0da0d6c9ba39d5fba17c",
+    "url": "https://franz101.github.io/reactHN/build/static/js/2.62510682.chunk.js"
+  },
+  {
+    "revision": "db596dab7f1c48f08fb3a9f9a4104be5",
+    "url": "https://franz101.github.io/reactHN/build/static/js/2.62510682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6808fa73c0b8530363",
+    "url": "https://franz101.github.io/reactHN/build/static/js/main.367d4771.chunk.js"
+  },
+  {
+    "revision": "21de929e6ec202f18ad5",
+    "url": "https://franz101.github.io/reactHN/build/static/js/runtime-main.2c3bbe8d.js"
+  }
+]);
